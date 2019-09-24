3f5f53beff9e8cd9d86e9dc5afe5127b27558d8c
sarytky <santeri.rytky@student.oulu.fi>
2019-09-24 10:39:12 +0300

    Metric evaluation script - results are saved to an Excel

--- scripts/evaluate metrics.py
+++ scripts/evaluate metrics.py
@@ -5,9 +5,10 @@ from pathlib import Path
 import argparse
 import dill
 import yaml
+import pandas as pd
 from time import sleep, time
 
-from rabbitccs.data.utilities import load, save
+from rabbitccs.data.utilities import load
 from rabbitccs.data.visualizations import render_volume
 
 from deeppipeline.segmentation.evaluation.metrics import calculate_iou, calculate_dice, \
@@ -20,17 +21,19 @@ cv2.setNumThreads(0)
 
 if __name__ == "__main__":
     start = time()
+    pred_path = Path('/media/dios/dios2/RabbitSegmentation/µCT/predictions_5_fold_trainingset')
+    snapshot = Path('dios-erc-gpu_2019_09_18_15_32_33_8samples')
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--mask_path', type=Path, default='../../../Data/µCT/masks')
-    parser.add_argument('--prediction_path', type=Path, default='/media/dios/dios2/RabbitSegmentation/µCT/predictions_5_fold_trainingset')
-    parser.add_argument('--save_dir', type=Path, default='/media/dios/dios2/RabbitSegmentation/µCT/predictions_5_fold_trainingset/evaluation')
+    parser.add_argument('--prediction_path', type=Path, default=pred_path)
+    parser.add_argument('--save_dir', type=Path, default=pred_path / 'evaluation')
     parser.add_argument('--n_threads', type=int, default=16)
     parser.add_argument('--n_labels', type=int, default=2)
     parser.add_argument('--weight', type=str, choices=['pyramid', 'mean'], default='mean')
     parser.add_argument('--experiment', default='./experiment_config_uCT.yml')
     parser.add_argument('--snapshot', type=Path,
-                        default='../../../workdir/snapshots/dios-erc-gpu_2019_09_18_15_32_33_8samples/')
+                        default=Path('../../../workdir/snapshots/') / snapshot)
     parser.add_argument('--dtype', type=str, choices=['.bmp', '.png', '.tif'], default='.bmp')
     args = parser.parse_args()
 
@@ -44,6 +47,8 @@ if __name__ == "__main__":
     with open(args.snapshot / 'split_config.dill', 'rb') as f:
         split_config = dill.load(f)
 
+    # Initialize results
+    results = {'Sample': [], 'Dice': [], 'IoU': [], 'Similarity': []}
 
     # Loop for samples
     args.save_dir.mkdir(exist_ok=True)
@@ -54,20 +59,42 @@ if __name__ == "__main__":
             sleep(0.5); print(f'==> Processing sample: {sample}')
 
             # Load image stacks
-            mask, files_mask = load(str(args.mask_path / sample), rgb=False)
-            pred, files_pred = load(str(args.prediction_path / sample), rgb=False)
+            mask, files_mask = load(str(args.mask_path / sample), rgb=False, n_jobs=args.n_threads)
+            pred, files_pred = load(str(args.prediction_path / sample), rgb=False, n_jobs=args.n_threads)
 
             conf_matrix = calculate_conf(pred.astype(np.bool), mask.astype(np.bool), args.n_labels)
             dice = calculate_dice(conf_matrix)[1]
             iou = calculate_iou(conf_matrix)[1]
+            sim = calculate_volumetric_similarity(conf_matrix)[1]
 
-            print(f'Sample {sample}: dice = {dice}, IoU = {iou}')
+            print(f'Sample {sample}: dice = {dice}, IoU = {iou}, similarity = {sim}')
 
             # Save predicted full mask
             render_volume(np.bitwise_xor(pred, mask),
                           savepath=str(args.save_dir / 'visualizations' / (sample + '_difference.png')),
                           white=False, use_outline=False)
+
+            # Update results
+            results['Sample'].append(sample)
+            results['Dice'].append(dice)
+            results['IoU'].append(iou)
+            results['Similarity'].append(sim)
+
+
         except ValueError:
             print(f'Sample {sample} failing, dimensions not consistent!')
             continue
-    print(f'Inference completed in {(time() - start) // 60} minutes, {(time() - start) % 60} seconds.')
+
+    # Add average value to
+    results['Sample'].append('Average values')
+    results['Dice'].append(np.average(results['Dice']))
+    results['IoU'].append(np.average(results['IoU']))
+    results['Similarity'].append(np.average(results['Similarity']))
+
+    # Write to excel
+    writer = pd.ExcelWriter(str(args.save_dir / ('metrics_' + str(snapshot))) + '.xlsx')
+    df1 = pd.DataFrame(results)
+    df1.to_excel(writer, sheet_name='Metrics')
+    writer.save()
+
+    print(f'Metrics evaluated in {(time() - start) // 60} minutes, {(time() - start) % 60} seconds.')
