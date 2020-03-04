025709f99570ab4c920da4a535609365d6d9f1c3
Moctader <md.moctader@student.oulu.fi>
2020-03-04 16:00:04 +0200

    HMDS_

--- scripts/train_3d.py
+++ scripts/train_3d.py
@@ -67,7 +67,6 @@ if __name__ == "__main__":
                             val_callbacks=val_cbs,
                             device=device)
         strategy.run()
-
         # Manage memory
         del strategy
         del model
